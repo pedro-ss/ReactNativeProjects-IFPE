36c2e28ed21e123386127b1dfd230159f85b6def
pjess <pjess@discente.ifpe.edu.br>
2022-05-20 11:14:53 -0300

    implementando social login

--- Login.js
+++ Login.js
@@ -3,22 +3,20 @@ import React, { useState } from 'react';
 import { Text, View, TextInput, Button } from 'react-native';
 import styleSheet from './Styles';
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 const styles = styleSheet;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQut_g1mHCwtTIqbfla7EjQXMXJe-GsT8",
-  authDomain: "atividade-mobile.firebaseapp.com",
-  projectId: "atividade-mobile",
-  storageBucket: "atividade-mobile.appspot.com",
-  messagingSenderId: "1018896212987",
-  appId: "1:1018896212987:web:fb699bad7d82ab4520a15e",
-  measurementId: "G-V6Y3JSH6E2"
+  apiKey: "AIzaSyAgnmVvKGo21nXmjsaIXvqoSisFTvS9C7k",
+  authDomain: "projetinho-flat.firebaseapp.com",
+  projectId: "projetinho-flat",
+  storageBucket: "projetinho-flat.appspot.com",
+  messagingSenderId: "460072259628",
+  appId: "1:460072259628:web:63bb4bc2d85e46257a4189"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth();
 
 function loginFirebase() {
@@ -32,7 +30,6 @@ function loginFirebase() {
       const errorMessage = error.message;
       console.log(errorMessage,errorCode)
     });
-
 }
 
 function cadastrarUsuario() {
@@ -47,6 +44,34 @@ function cadastrarUsuario() {
       console.log(errorMessage,errorCode)
     });
 }
+
+
+const google = new GoogleAuthProvider()
+const facebook = new FacebookAuthProvider()
+
+function socialLogin(provedor) {
+  signInWithPopup(auth, provedor)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = provedor.credentialFromResult(result);
+
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+      navigation.navigate('Listagem')
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = provedor.credentialFromError(error);
+      // ...
+    });
+}
+
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
@@ -77,6 +102,13 @@ export default function Login({ navigation }) {
 
       <View style={styles.loginButtons}>
         <Button title="Cadastre-se" accessibilityLabel="Cadastre-se" onPress={() => createUserWithEmailAndPassword(auth, email, senha)} color="#ff4500" />
+      </View>
+
+      <View style={styles.loginButtons}>
+        <Button title="Google" accessibilityLabel="Google" onPress={() => {socialLogin(google)}} color="#ff0000" />
+      </View>
+      <View style={styles.loginButtons}>
+        <Button title="Facebook" accessibilityLabel="Facebook" onPress={() => {socialLogin(facebook)}} color="#1877f2" />
       </View>
     </View>
   )
